5137e6dbca20f6aed73e781ddbfc41a341680cff
luideoz <Luis.Benito@alu.uclm.es>
2024-10-08 14:57:05 +0200

    primera implementacion del servicio y estrcutracion de ficheros

--- auth.py
+++ auth.py
@@ -4,3 +4,33 @@ import json
 class AuthService:
     def __init__(self):
         self.users = json.loads(open('users/users.json').read())
+    
+    def get_user(self, id) -> Dict:
+        for user in self.users:
+            if user["id"] == id:
+                user_json = json.dumps(user)
+                return user_json
+        return None
+    
+    def add_user(self, user: Dict) -> None:
+        id = self.users[-1]["id"] + 1
+        user["id"] = id
+        self.users.append(user)
+        with open('users/users.json', 'w') as f:
+            json.dump(self.users, f)
+
+    def delete_user(self, id) -> None:
+        for user in self.users:
+            if user["id"] == id:
+                self.users.remove(user)
+                with open('users/users.json', 'w') as f:
+                    json.dump(self.users, f)
+                return
+    
+    def update_user(self, id, user: Dict) -> None:
+        for i in range(len(self.users)):
+            if self.users[i]["id"] == id:
+                self.users[i] = user
+                with open('users/users.json', 'w') as f:
+                    json.dump(self.users, f)
+                return
