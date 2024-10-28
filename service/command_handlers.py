034b21670c8ae138f076d1660a4acb7ea717177e
luideoz <Luis.Benito@alu.uclm.es>
2024-10-28 18:00:06 +0100

    agregacion de parametros en la entrada estandar

--- service/command_handlers.py
+++ service/command_handlers.py
@@ -1,10 +1,21 @@
 from flask import Flask
 from service.service import AuthenticationService
 from service.authentication import auth
+import argparse
 
-def run_auth():
+
+def run_auth(host:str, port:int, db_route:str):
     app = Flask(__name__, instance_relative_config=True)
     app.config['service'] = AuthenticationService("users")
+    app.config['db_route'] = db_route
     app.register_blueprint(auth)
     app.run(host="0.0.0.0", port = 3000, debug=True)
+
+def args_handler():
+    parser = argparse.ArgumentParser(description='Run the authentication service')
+    parser.add_argument('--listening','-l', type=str, default='0.0.0.0', help='Host donde se pondra a la escucha el servidor')
+    parser.add_argument('--port','-p', type=int, default=3000, help='Puerto donde se pondra a la escucha el servidor')
+    parser.add_argument('--db', '-d', type=str, default='users/users.db', help='Ruta a la base de datos')
+    args = parser.parse_args()
+    run_auth(args.listening, args.port, args.db)
 
